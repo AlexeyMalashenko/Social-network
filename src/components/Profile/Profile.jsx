77d0281c611c269cdcc7f5888925aff0929281fb
AlexeyMalashenko <locks9lol@gmail.com>
2021-05-31 22:27:55 +0300

    login form /login npm i redux-form refact for formik

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
 import MyPostsContainer from "./MyPosts/MyPostsContainer";
-import {updateUserStatus} from "../../redux/profile-reducer";
 
 const Profile = (props) => {
     return (
